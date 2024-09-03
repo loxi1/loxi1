636ed2f448fe91c24439f97a3555f22fed0bdcce
Anibal Cayetano <anibal.cayetano@gmail.com>
2024-09-03 00:30:45 -0500

    Update sp.sql
    una, consulta mas

--- sp.sql
+++ sp.sql
@@ -1,3 +1,27 @@
+-- CREATE FUNCTION "updateop" ----------------------------------
+```js
+CREATE DEFINER=`usergoerp`@`%` PROCEDURE `updateop`(IN `idempx` INT)
+BEGIN
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE idop, id, idkar INT;
+  DECLARE cur1 CURSOR FOR select op.id_ordendeproduccion,op.id_documento, 
+(select id_kardex from tb_kardex ka where ka.id_art_sucursal=op.id_art_sucursal and ka.id_documento=op.id_documento and ka.id_tipomovimiento in (4,11,13,14) limit 1) idka
+from tb_ordendeproduccion op where op.id_documento>0 and op.estado in (7,8,9,10);
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    OPEN cur1;
+      read_loop: LOOP
+        FETCH cur1 INTO idop, id, idkar;
+        IF done THEN
+          LEAVE read_loop;
+        END IF;
+        IF  idop > 0 THEN
+            UPDATE tb_ordendeproduccion SET estado = 11  WHERE id_ordendeproduccion = idop;
+        END IF;
+      END LOOP;
+    CLOSE cur1;
+END;
+```
+-- -------------------------------------------------------------
 -- CREATE FUNCTION "up_op_rend" --------------------------------
 ```js
 CREATE DEFINER=`usergoerp`@`%` PROCEDURE `up_op_rend`()
